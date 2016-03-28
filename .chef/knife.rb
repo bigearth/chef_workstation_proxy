d6402ec6388e9562585d19cc3ba55832d5284457
Carlos Cardona <cgcardona@gmail.com>
2016-03-27 19:08:32 -0700

    Update env var.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,7 +5,7 @@ node_name                "#{ENV['CHEF_WORKSTATION_PROXY_NODE_NAME']}"
 client_key               "#{current_dir}/#{ENV['CHEF_WORKSTATION_PROXY_CLIENT_KEY']}.pem"
 validation_client_name   "#{ENV['CHEF_WORKSTATION_PROXY_VALIDATION_CLIENT_NAME']}"
 validation_key           "#{current_dir}/#{ENV['CHEF_WORKSTATION_PROXY_VALIDATION_KEY']}.pem"
-chef_server_url          "#{ENV['CHEF_WORKSTATION_PROXY_VALIDATION_KEY_CHEF_SERVER_URL']}"
+chef_server_url          "#{ENV['CHEF_WORKSTATION_PROXY_CHEF_SERVER_URL']}"
 syntax_check_cache_path  "#{ENV['HOME']}/.chef/syntaxcache"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 knife[:editor]="/usr/bin/vim"
