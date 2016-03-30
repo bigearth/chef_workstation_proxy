c94e40d036e6d086f1e8ba8dd904774f9fa60dc0
Carlos Cardona <cgcardona@gmail.com>
2016-03-30 14:25:07 -0700

    Update run-list and depends.

--- chef_repo/cookbooks/bitcoin/metadata.rb
+++ chef_repo/cookbooks/bitcoin/metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures the official Bitcoin Core'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.2'
 
-depends "apt"
+depends "bootstrap_node_generic"
 
 supports 'centos'
 supports 'debian'
--- chef_workstation_proxy.rb
+++ chef_workstation_proxy.rb
@@ -13,15 +13,11 @@ module BigEarth
         username == ENV['CHEF_WORKSTATION_PROXY_USERNAME'] and password == ENV['CHEF_WORKSTATION_PROXY_PASSWORD']
       end
       
-      get '/' do
-        puts 'Hello World'
-      end
-
       post '/bootstrap_chef_client' do
         begin
           data = JSON.parse request.body.read
           flavor = data['flavor']
-          system "cd ~/chef_workstation_proxy/chef_repo && knife bootstrap #{data['ipv4_address']} -x root -A -P password --sudo --use-sudo-password -N #{data['title'].tr(' ', '_')} -r 'recipe[chef_workstation_proxy]'"
+          system "cd ~/chef_workstation_proxy/chef_repo && knife bootstrap #{data['ipv4_address']} -x root -A -P password --sudo --use-sudo-password -N #{data['title'].tr(' ', '_')} -r 'recipe[bootstrap_node_generic], recipe[bitcoin::bitcoin_#{flavor}]'"
           system "cd ~/chef_workstation_proxy/chef_repo && ssh root@#{data['ipv4_address']} 'sudo chef-client'"
         rescue => error
           puts "[ERROR] #{Time.now}: #{error.class}: #{error.message}"
