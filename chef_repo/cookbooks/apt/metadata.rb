63af842b0ebd14615994e17b14fbc7860e4de7a3
Carlos Cardona <cgcardona@gmail.com>
2016-03-30 14:34:34 -0700

    Remove 'apt' cookbook and all dependencies.

--- chef_repo/cookbooks/apt/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'apt'
-maintainer       'Gabriel Cardona'
-maintainer_email 'gabr13l@bigearth.io'
-license          'MIT License'
-description      'Installs/Configures apt'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
