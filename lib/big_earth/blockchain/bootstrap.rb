14ff5917231118c306450511aca0e1fcd6ab6d17
Carlos Cardona <cgcardona@gmail.com>
2016-03-31 13:01:17 -0700

    Wrapped more chef calls in Ruby classes and pulled out path to chef_repo into env var.

--- lib/big_earth/blockchain/bootstrap.rb
+++ lib/big_earth/blockchain/bootstrap.rb
@@ -6,11 +6,20 @@ module BigEarth
           @data = data
         end
         
-        def bootstrap
+        def bootstrap fqdn = '', options = ''
           flavor = @data['flavor']
-          system "cd ~/chef_workstation_proxy/chef_repo && knife bootstrap #{@data['ipv4_address']} -x root -A -P password --sudo --use-sudo-password -N #{@data['title'].tr(' ', '_')} -r 'recipe[bootstrap_node_generic], recipe[bitcoin::bitcoin_#{flavor}]'"
+          system "cd #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife bootstrap #{@data['ipv4_address']} -x root -A -P password --sudo --use-sudo-password -N #{@data['title'].tr(' ', '_')} -r 'recipe[bootstrap_node_generic], recipe[bitcoin::bitcoin_#{flavor}]'"
         end
         
+        def bootstrap_windows_ssh fqdn = '', options = ''
+          system "cd  #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife bootstrap windows ssh #{fqdn} #{options}"
+        end
+        
+        def bootstrap_windows_winrm fqdn = '', options = ''
+          system "cd  #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife bootstrap windows winrm #{fqdn} #{options}"
+        end
+        
+        # TODO move chef_client from this class
         def chef_client 
           system "cd ~/chef_workstation_proxy/chef_repo && ssh root@#{@data['ipv4_address']} 'sudo chef-client'"
         end
--- lib/big_earth/blockchain/node.rb
+++ lib/big_earth/blockchain/node.rb
@@ -3,47 +3,47 @@ module BigEarth
     module Knife 
       class Node
         def bulk_delete regex = '', options = '' 
-          system "cd ~/chef_workstation_proxy/chef_repo && knife node bulk delete #{regex} #{options}"
+          system "cd #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife node bulk delete #{regex} #{options}"
         end
         
         def create node = '', options = ''
-          system "cd ~/chef_workstation_proxy/chef_repo && knife node create #{node} #{options}"
+          system "cd #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife node create #{node} #{options}"
         end
         
         def delete node = '', options = ''
-          system "cd ~/chef_workstation_proxy/chef_repo && knife node delete #{node} #{options}"
+          system "cd #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife node delete #{node} #{options}"
         end
         
         def edit node = '', options = ''
-          system "cd ~/chef_workstation_proxy/chef_repo && knife node edit #{node} #{options}"
+          system "cd #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife node edit #{node} #{options}"
         end
         
         def environment_set node = '', environment = ''
-          system "cd ~/chef_workstation_proxy/chef_repo && knife node environment set #{node} #{environment}"
+          system "cd #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife node environment set #{node} #{environment}"
         end
         
         def from_file file = '', options = ''
-          system "cd ~/chef_workstation_proxy/chef_repo && knife node from file #{file} #{options}"
+          system "cd #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife node from file #{file} #{options}"
         end
         
         def list options = ''
-          system "cd ~/chef_workstation_proxy/chef_repo && knife node list #{options}"
+          system "cd #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife node list #{options}"
         end
         
         def run_list_add node = '', entry = '', options = ''
-          system "cd ~/chef_workstation_proxy/chef_repo && knife node run_list add #{node} #{entry} #{options}"
+          system "cd #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife node run_list add #{node} #{entry} #{options}"
         end
         
         def run_list_remove node = '', entry = '', options = ''
-          system "cd ~/chef_workstation_proxy/chef_repo && knife node run_list remove #{node} #{entry} #{options}"
+          system "cd #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife node run_list remove #{node} #{entry} #{options}"
         end
         
         def run_list_set node = '', entries = '', options = ''
-          system "cd ~/chef_workstation_proxy/chef_repo && knife node run_list remove #{node} #{entries} #{options}"
+          system "cd #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife node run_list remove #{node} #{entries} #{options}"
         end
         
         def show node = '', options = ''
-          system "cd ~/chef_workstation_proxy/chef_repo && knife node run_list remove #{node} #{options}"
+          system "cd #{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']} && knife node run_list remove #{node} #{options}"
         end
       end
     end
