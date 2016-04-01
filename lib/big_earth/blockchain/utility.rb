3c011bffc4ec0e90bb4eee396eddba13bc2611a3
Carlos Cardona <cgcardona@gmail.com>
2016-04-01 16:00:50 -0700

    Wrap Berkshelf in a Ruby class.

--- lib/big_earth/blockchain/utility.rb
+++ lib/big_earth/blockchain/utility.rb
@@ -3,16 +3,17 @@ module BigEarth
     module Utility
       def update_cookbooks recipes
         require 'cookbook'
+        require 'berkshelf'
         # Instance of Cookbook
         cookbook = BigEarth::Blockchain::Knife::Cookbook.new
         
-        # TODO bundle berkshelf dependencies
-        # berks install
-        # TODO upload berkshelf dependencies to chef-server
-        # berks install
-        
         # upload the latest and greatest of each cookbook to chef-server
         recipes.each do |recipe|
+          puts "RECIPE #{recipe}"
+          berkshelf = BigEarth::Blockchain::Berkshelf.new
+          puts "Berkshelf: #{berkshelf}"
+          berkshelf.install if File.file? "#{ENV['CHEF_WORKSTATION_PROXY_CHEF_REPO_PATH']}/cookbooks/#{recipe}" 
+          berkshelf.upload('-b /.Berksfile') if File.file? '/.Berksfile' 
           cookbook.upload recipe 
         end
       end
