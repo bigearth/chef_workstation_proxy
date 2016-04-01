ae4dd5248fcbdee0a66609e3de16ce29ab09ae12
Carlos Cardona <cgcardona@gmail.com>
2016-04-01 08:15:34 -0700

    Clean up and add error message.

--- lib/workers/bootstrap_chef_client.rb
+++ lib/workers/bootstrap_chef_client.rb
@@ -6,17 +6,16 @@ module BigEarth
       @queue = '_bootstrap_chef_client_worker'
       
       def self.perform config 
-        puts "DATA #{config}"
         require 'bootstrap'
         begin
           require 'node'
           node = BigEarth::Blockchain::Knife::Node.new
-          puts node.list
           bootstrap = BigEarth::Blockchain::Knife::Bootstrap.new
           bootstrap.bootstrap config['options']['ipv4_address'], config
           bootstrap.chef_client 
         rescue => error
-            puts "[ERROR] #{Time.now}: #{error.class}: #{error.message}"
+          puts 'bootstrap chef client error'
+          puts "[ERROR] #{Time.now}: #{error.class}: #{error.message}"
         end
       end
     end
