1f2bada599cff4a61bea82a0e370aa045740430b
Carlos Cardona <cgcardona@gmail.com>
2016-03-31 18:50:28 -0700

    Include apt dependency.

--- chef_repo/cookbooks/bootstrap_node_generic/metadata.rb
+++ chef_repo/cookbooks/bootstrap_node_generic/metadata.rb
@@ -6,3 +6,4 @@ description      'To bring up and configure an Ubuntu instance for use in the Bi
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 supports         'ubuntu', '>= 14.04'
+depends 'apt'
