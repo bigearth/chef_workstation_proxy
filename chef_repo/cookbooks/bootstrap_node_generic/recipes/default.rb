1be4c2441f77820bbec1e7e6ac20f1fa4ae70fb3
Carlos Cardona <cgcardona@gmail.com>
2016-04-02 20:05:10 -0700

    inline password shadow hash.

--- chef_repo/cookbooks/bootstrap_node_generic/recipes/default.rb
+++ chef_repo/cookbooks/bootstrap_node_generic/recipes/default.rb
@@ -10,10 +10,14 @@
 # Run apt-get update
 include_recipe 'apt'
 
+require 'digest/sha2'
+password = "pass@123"
+salt = rand(36**8).to_s(36)
+shadow_hash = password.crypt("$6$" + salt)
 # add user
 user node['bootstrap_node_generic']['user'] do
   home node['bootstrap_node_generic']['source']['home']
-  password password_shadow_hash
+  password shadow_hash
   # shell "/usr/bin/zsh"
   shell "/bin/bash"
   supports manage_home: true
