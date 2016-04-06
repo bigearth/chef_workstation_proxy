2465872028b914063d833386f43494a81bbc0786
Carlos Cardona <cgcardona@gmail.com>
2016-04-06 11:42:09 -0700

    Remove extra installed packages.

--- chef_repo/cookbooks/chef_client/attributes/default.rb
+++ chef_repo/cookbooks/chef_client/attributes/default.rb
@@ -1,8 +1,5 @@
 default['chef_client']['source']['home'] = "/home/#{node['chef_client']['user']}"
-default['chef_client']['source']['dependencies'] = [
-  'build-essential', 'autoconf', 'libssl-dev', 'libboost-dev', 'pkg-config',
-  'libboost-chrono-dev', 'libboost-filesystem-dev', 'libboost-program-options-dev',
-  'libboost-system-dev', 'libboost-test-dev', 'libboost-thread-dev', 'libtool',
-  'tmux', 'vim', 'tree', 'ack-grep', 'ntp', 'git', 'libdb-dev', 'libdb++-dev', 'libboost-all-dev',
-  'libqrencode-dev', 'htop', 'bundler', 'zsh', 'git-core', 'tig', 'unzip', 'makepasswd',
-  'transmission-common', 'transmission-daemon', 'transmission-remote-cli']
+default['chef_client']['source']['dependencies'] = [ 
+  'build-essential', 'autoconf', 'tmux', 'vim', 'tree', 
+  'ack-grep', 'ntp', 'git', 'htop', 'bundler', 'zsh', 'tig' 
+]
