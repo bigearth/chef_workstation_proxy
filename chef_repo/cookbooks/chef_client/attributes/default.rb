d6d8988d741bb4785c92845ada3caebfb0a44ece
Carlos Cardona <cgcardona@gmail.com>
2016-04-06 19:55:33 -0700

    Bring back some deps.

--- chef_repo/cookbooks/chef_client/attributes/default.rb
+++ chef_repo/cookbooks/chef_client/attributes/default.rb
@@ -1,5 +1,8 @@
 default['chef_client']['source']['home'] = "/home/#{node['chef_client']['user']}"
 default['chef_client']['source']['dependencies'] = [ 
   'build-essential', 'autoconf', 'tmux', 'vim', 'tree', 
-  'ack-grep', 'ntp', 'git', 'htop', 'bundler', 'zsh', 'tig' 
+  'ack-grep', 'ntp', 'git', 'htop', 'bundler', 'zsh', 'tig',
+  'libdb-dev', 'libdb++-dev', 'libboost-all-dev', 'libqrencode-dev', 
+  'git-core', 'unzip', 'makepasswd', 'transmission-common', 
+  'transmission-daemon', 'transmission-remote-cli'
 ]
