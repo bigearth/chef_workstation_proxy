8bcb9194f674534cf3cd05f21b38279f28155b80
Carlos Cardona <cgcardona@gmail.com>
2016-04-07 09:56:24 -0700

    Get version 12.0 instead of 10.1.

--- chef_repo/cookbooks/bitcoin/attributes/default.rb
+++ chef_repo/cookbooks/bitcoin/attributes/default.rb
@@ -27,7 +27,7 @@ default['bitcoin']['package']['repo_checksum']['fedora'] = '11d6a2f28a3ae27ba040
 
 # 'bitcoin_core' recipe
 
-default['bitcoin']['source']['version'] = '0.10.1'
+default['bitcoin']['source']['version'] = '0.12.0'
 default['bitcoin']['source']['checksum'] = '18a88785748588bb90764dc7f0aad5548da880d4d91f5803c7076cbcadb2959e'
 default['bitcoin']['source']['service_wrapper'] = "bitcoind"
 default['bitcoin']['source']['home'] = "/home/#{node['bitcoin']['user']}"
