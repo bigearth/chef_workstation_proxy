4ac22a873bac41809c59315f81cb074091606a38
Carlos Cardona <cgcardona@gmail.com>
2016-04-07 18:08:07 -0700

    Update configure options.

--- chef_repo/cookbooks/bitcoin/attributes/default.rb
+++ chef_repo/cookbooks/bitcoin/attributes/default.rb
@@ -34,7 +34,7 @@ default['bitcoin']['source']['home'] = "/home/#{node['bitcoin']['user']}"
 default['bitcoin']['source']['prefix'] = "/usr/local"
 default['bitcoin']['source']['bitcoind'] = "#{node['bitcoin']['source']['prefix']}/bin/bitcoind"
 default['bitcoin']['source']['bitcoin-cli'] = "#{node['bitcoin']['source']['prefix']}/bin/bitcoin-cli"
-default['bitcoin']['source']['configure_options'] = "--with-incompatible-bdb --without-gui"
+default['bitcoin']['source']['configure_options'] = "--disable-wallet --without-gui --prefix=#{node['bitcoin']['source']['prefix']}"
 default['bitcoin']['source']['make_options'] = "-j#{node['cpu']['total']}"
 default['bitcoin']['source']['dependencies'] = [
   'build-essential', 'autoconf', 'libssl-dev', 'libboost-dev', 'pkg-config',
