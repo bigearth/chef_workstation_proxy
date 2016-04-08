78012dee7b26bcc2efe433d3fad41d5848552c82
Carlos Cardona <cgcardona@gmail.com>
2016-04-07 17:52:51 -0700

    Make v0.12.0.

--- chef_repo/cookbooks/bitcoin/attributes/default.rb
+++ chef_repo/cookbooks/bitcoin/attributes/default.rb
@@ -27,16 +27,17 @@ default['bitcoin']['package']['repo_checksum']['fedora'] = '11d6a2f28a3ae27ba040
 
 # 'bitcoin_core' recipe
 
-default['bitcoin']['source']['version'] = '0.10.1'
+default['bitcoin']['source']['version'] = '0.12.0'
 default['bitcoin']['source']['checksum'] = '18a88785748588bb90764dc7f0aad5548da880d4d91f5803c7076cbcadb2959e'
 default['bitcoin']['source']['service_wrapper'] = "bitcoind"
 default['bitcoin']['source']['home'] = "/home/#{node['bitcoin']['user']}"
 default['bitcoin']['source']['prefix'] = "/usr/local"
 default['bitcoin']['source']['bitcoind'] = "#{node['bitcoin']['source']['prefix']}/bin/bitcoind"
 default['bitcoin']['source']['bitcoin-cli'] = "#{node['bitcoin']['source']['prefix']}/bin/bitcoin-cli"
-default['bitcoin']['source']['configure_options'] = "--disable-wallet --without-gui --prefix=#{node['bitcoin']['source']['prefix']}"
+default['bitcoin']['source']['configure_options'] = "--with-incompatible-bdb --without-gui"
 default['bitcoin']['source']['make_options'] = "-j#{node['cpu']['total']}"
 default['bitcoin']['source']['dependencies'] = [
   'build-essential', 'autoconf', 'libssl-dev', 'libboost-dev', 'pkg-config',
   'libboost-chrono-dev', 'libboost-filesystem-dev', 'libboost-program-options-dev',
-  'libboost-system-dev', 'libboost-test-dev', 'libboost-thread-dev', 'libtool']
+  'libboost-system-dev', 'libboost-test-dev', 'libboost-thread-dev', 'libtool',
+  'libboost-all-dev', 'libprotobuf-dev', 'protobuf-compiler', 'libqt4-dev', 'libqrencode-dev'] 
