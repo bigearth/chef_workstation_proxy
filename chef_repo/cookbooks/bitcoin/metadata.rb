294b0ec62fd4903e61c12002adab37240f622201
Carlos Cardona <cgcardona@gmail.com>
2016-04-10 10:28:03 -0700

    Update recipe info.

--- chef_repo/cookbooks/bitcoin/metadata.rb
+++ chef_repo/cookbooks/bitcoin/metadata.rb
@@ -1,15 +1,11 @@
 name             'bitcoin'
-maintainer       'Cédric Félizard'
-maintainer_email 'cedric@felizard.fr'
-license          'MIT'
-description      'Installs/Configures the official Bitcoin Core'
+maintainer       'Gabriel Cardona'
+maintainer_email 'gabr13l@bigearth.io'
+license          'GPLv2'
+description      'Installs/Configures the Bitcoin Core/XT/Classic'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.2'
+version          '0.1.0'
 
 depends "chef_client"
 
-supports 'centos'
-supports 'debian'
-supports 'fedora'
-supports 'redhat'
 supports 'ubuntu'
