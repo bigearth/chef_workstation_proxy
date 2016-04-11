fb85d3ed121dd7efb80c98e08cf17e2e1e924af8
Carlos Cardona <cgcardona@gmail.com>
2016-04-11 12:23:26 -0700

    Bump version of cookbook.

--- chef_repo/cookbooks/chef_client/metadata.rb
+++ chef_repo/cookbooks/chef_client/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'gabr13l@bigearth.io'
 license          'GPL v2'
 description      'To bring up and configure an Ubuntu instance for use in the Big Earth ecosystem'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 supports         'ubuntu', '>= 14.04'
 depends          'apt'
 depends          'git'
